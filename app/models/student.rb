108fe0e0b0ff41c4da50b9f43ea68c44fbd59c0d
Caroline Shields <cbshields@outlook.com>
2018-04-16 22:31:23 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,12 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    if query != ""
+      student = Student.find_by(name: query)
+      render "index"
+    else
+      self.all
+  end
 end
